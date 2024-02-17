9df4a13d69e2f46723139da87d4172a30c668490
Aashishkumar021 <88232290+Aashishkumar021@users.noreply.github.com>
2024-02-17 09:00:22 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144
+let accountEmail = "aashish@gmial.com"
+var accountPassword ="58975"
+ accountCity ="Jaipur"//we can assign memory to a variable without stating its data type though not recommended
+ let accountsomething; //this will give output as UNDEFINED 
+
+ //acountId= change not allowed as it is constant 
+
+ acccountEmail="anything@gmail.com"
+ accountPassword="saskjnf1"
+ accountCity="bengaluru"
+
+ /*
+prefer not to use var
+instead we use let
+because of issue in functional scope and block scope
+
+ */
+
+ console.log([accountId,accountEmail,accountPassword,accountCity])
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict " // after using this code will be treated as the new version of node js
+
+//alert(3+3) cannot be used but some other way to use in node 
+
+console.log(3
+    +3);  //this will work but code readability gets bad so not recommended
+
+//tc39.es has the actual standards for js
+
+/*
+number =>2 to power 53
+bigint
+string
+boolean =>true/false
+null=>standalone value
+undefined=>
+symbol=> to identify uniqueness
+
+*/
+
+console.log(typeof null);//null has datatype OBJECT
+//Undefined is UNDEFINED datatype itself
